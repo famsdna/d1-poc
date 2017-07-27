2ab867262a40f89f30bbc4b5c9bded523848973d
Ruben Rodriguez <rurodriguez@firstam.com>
2017-07-26 21:59:29 -0400

    testing change

--- providers/d1c-git/d1c-git.tf
+++ providers/d1c-git/d1c-git.tf
@@ -5,6 +5,19 @@ variable "terraform_template"     { default =   "d1c-git" }
 variable "region"              { }
 variable "role_arn"              { }
 
+terraform {
+  backend "s3" {
+    bucket = "cfn-buildscripts"
+    region = "us-east-1"
+    key = "d1c-git/terraform.tfstate"
+    profile = "default"
+  }
+}
+
+atlas {
+  name = "fams/git-d1c-poc"
+}
+
 provider "aws" {
   region = "${var.region}"
   assume_role {
@@ -14,7 +27,7 @@ provider "aws" {
 
 resource "aws_vpc_dhcp_options" "foo" {
   domain_name          = "service.consul"
-  domain_name_servers  = ["127.0.0.1", "10.0.0.2"]
+  domain_name_servers  = ["127.0.0.1", "10.0.0.3"]
   ntp_servers          = ["127.0.0.1"]
   netbios_name_servers = ["127.0.0.1"]
   netbios_node_type    = 2
